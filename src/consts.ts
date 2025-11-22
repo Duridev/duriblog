c7a4f00bbb5fc13bda35f3d21085a827be5c6d6f
duridev <duridev@outlook.com>
2025-11-22 01:55:42 -0300

    feat: Update site details and enhance homepage content for Duriblog

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Duriblog';
+export const SITE_DESCRIPTION = 'Reflexiones sobre la vida, espiritualidad y pensamientos para mi hijo';
